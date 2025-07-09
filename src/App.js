0a5fda20b4a00a0d2b36c06f7c035253bdf0edb2
Avinashchidurala <chiduralaavinash@gmail.com>
2025-07-10 04:36:36 +0530

    = ==react version of ui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState('');
